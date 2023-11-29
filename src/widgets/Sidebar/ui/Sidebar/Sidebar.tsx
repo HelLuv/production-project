44da4baeea4d1ba17282500e9f77e262170bbaf3
HelLuv <works.person@gmail.com>
2023-11-29 20:47:29 +0600

    26-27 lessons: added loki screenshot testing, github actions and ci

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import { ThemeSwitcher } from 'widgets/ThemeSwitcher';
 import { LangSwitcher } from 'widgets/LangSwitcher';
 import { Button } from 'shared/ui/Button/Button';
+import { useTranslation } from 'react-i18next';
 import cls from './Sidebar.module.scss';
 
 interface SidebarProps {
@@ -10,6 +11,7 @@ interface SidebarProps {
 }
 
 export const Sidebar = ({ className }: SidebarProps) => {
+    const { t } = useTranslation();
     const [collapsed, setCollapsed] = useState(false);
 
     function onToggle() {
@@ -26,7 +28,7 @@ export const Sidebar = ({ className }: SidebarProps) => {
                 onClick={onToggle}
                 data-testid="sidebar-toggle"
             >
-                TOGGLE
+                {t('TOGGLE')}
             </Button>
             <div className={cls.switchers}>
                 <ThemeSwitcher />
