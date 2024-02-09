d3406e8e069cdc08cf03dafddfed059ee773bff5
HelLuv <works.person@gmail.com>
2024-02-09 12:58:05 +0700

    93 lesson: updated eslint plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -67,7 +67,7 @@ module.exports = {
         'react-hooks/exhaustive-deps': 'error',
         'no-param-reassign': 'off',
         'no-new': 'warn',
-        'import-path-checker/path-checker': 'error',
+        'import-path-checker/path-checker': ['error', { alias: '@' }],
     },
     globals: {
         __IS_DEV__: true,
