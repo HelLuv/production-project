f833cf895dfe65616912b48e92a72fbcdc5abde8
HelLuv <works.person@gmail.com>
2024-02-10 14:46:08 +0700

    104 lesson: clear babel-loader cache script, postinstall hook

---
+++ scripts/clear-cache.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const path = require('path');
+
+const cacheDir = path.join(__dirname, '..', 'node_modules/.cache');
+fs.rmSync(cacheDir, { recursive: true, force: true });
+
+console.log('Cache is cleared');
